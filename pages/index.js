f205eae597b56145e1f17fce5d620713aa3b30fe
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-02-18 23:57:15 -0300

    The welcome message for the app has been created on the home screen.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,12 @@ export default function Home() {
   return (
     <>
     <Header/>
+    <div className="flex flex-col items-center justify-center pt-12">
+      <h1 className="text-5xl font-bold mb-10">Agenda Veterinaria</h1>
+      <div className="flex items-center">
+        <img src="/VetPlanner.png" alt="Logo" className="w-32 h-32 mr-2"/>
+      </div>
+    </div>
     <Footer/>
     </>
   );
