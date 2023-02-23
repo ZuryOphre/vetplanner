e4153f6d744c04b986a0aa6eb1eb7bd06593f126
Zury Martinez <zury.martinez.saldivia@gmail.com>
2023-02-23 20:30:07 -0300

    The schema.prisma was modified.
    The schema.prisma was modified.

--- prisma/migrations/20230223231858_vetplannerfix/migration.sql
+++ prisma/migrations/20230223231858_vetplannerfix/migration.sql
@@ -13,9 +13,10 @@ CREATE TABLE "Owner" (
 CREATE TABLE "Pet" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "age" INTEGER NOT NULL,
-    "diagnosis" TEXT NOT NULL,
-    "instructions" TEXT NOT NULL,
+    "age" INTEGER,
+    "diagnosis" TEXT,
+    "instructions" TEXT,
+    "observations" TEXT NOT NULL,
     "admission" TIMESTAMP(3) NOT NULL,
     "discharge" TIMESTAMP(3),
     "ownerId" INTEGER NOT NULL,
@@ -23,5 +24,18 @@ CREATE TABLE "Pet" (
     CONSTRAINT "Pet_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Appointment" (
+    "id" SERIAL NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "veterinarian" TEXT NOT NULL,
+    "petId" INTEGER NOT NULL,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
+);
+
 -- AddForeignKey
 ALTER TABLE "Pet" ADD CONSTRAINT "Pet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Owner"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_petId_fkey" FOREIGN KEY ("petId") REFERENCES "Pet"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
